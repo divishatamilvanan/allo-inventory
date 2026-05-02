9b1dd05706cc50760f4839bad6eee6c216990f7f
Divisha Tamilvanan <divishatamilvanan@gmail.com>
2026-05-02 15:44:56 +0530

    fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  images: { domains: ['images.unsplash.com'] },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
-
 module.exports = nextConfig;
