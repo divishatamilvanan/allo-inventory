5ce78a4f3ba654c5f12b685fda816ee24d0ecf2c
Divisha Tamilvanan <divishatamilvanan@gmail.com>
2026-05-02 15:53:48 +0530

    fix prisma build issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['images.unsplash.com'] },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 };
+
 module.exports = nextConfig;
